c6d68edee73369e368e67ebb44508c47aba76ed8
Victor-Onanuga <44228722+Victor-Onanuga@users.noreply.github.com>
2020-10-18 19:07:47 +0100

    Lab3 upload

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Header extends React.Component{
+
+    render(){    
+        return(
+            <div>
+                <h1>This is the Header Component.</h1>
+            </div>
+        );
+    }
+}
